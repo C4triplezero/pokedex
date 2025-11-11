3b5d55e639015183563785b02bc2cd38ab9838b8
C4triplezero <jcurrie3000@gmail.com>
2025-11-11 02:51:24 -0700

    added cli interaction functionality with help and exit commands

--- repl.go
+++ repl.go
@@ -1,10 +1,77 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"strings"
 )
+
+func startRepl() error {
+	reader := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		reader.Scan()
+
+		words := cleanInput(reader.Text())
+		if len(words) == 0 {
+			continue
+		}
+		input := words[0]
+
+		command, ok := getCommands()[input]
+		if !ok {
+			println("Unknown command")
+			continue
+		}
+		err := command.callback()
+		if err != nil {
+			fmt.Println(err)
+		}
+
+	}
+}
 
 func cleanInput(text string) []string {
 	output := strings.Fields(strings.ToLower(text))
 	return output
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+	}
+}
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func commandHelp() error {
+	fmt.Println("\nWelcome to the Pokedex!\nUsage:")
+	fmt.Println()
+	fmt.Println()
+
+	for _, cmd := range getCommands() {
+		fmt.Printf("%v: %v\n", cmd.name, cmd.description)
+	}
+
+	return nil
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
 }
